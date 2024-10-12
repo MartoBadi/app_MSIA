bc58b9e8cf96abb2c1c87bed5544414aadaf0707
Martín Badino <151761027+MartoBadi@users.noreply.github.com>
2024-10-11 22:28:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,49 @@
 import streamlit as st
 import tensorflow as tf
-from PIL import Image, ImageOps
+import cv2
 import numpy as np
 
-# Load the trained model
-model = tf.keras.models.load_model('model.tflite')
+# Cargar el modelo TFLite
+interpreter = tf.lite.Interpreter(model_path='model.tflite')
+interpreter.allocate_tensors()
 
-# Define class names
+# Obtener detalles de entrada y salida
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Definir nombres de clases
 class_names = ['burj_khalifa', 'chichen_itza', 'christ the reedemer', 'eiffel_tower', 'great_wall_of_china', 'machu_pichu', 'pyramids_of_giza', 'roman_colosseum', 'statue_of_liberty', 'stonehenge', 'taj_mahal', 'venezuela_angel_falls']
 
-# Preprocess the uploaded image
+# Preprocesar la imagen subida
 def preprocess_image(image):
     size = (150, 150)
-    image = ImageOps.fit(image, size, Image.LANCZOS)
-    img_array = np.asarray(image)
-    img_array = img_array / 255.0  # Normalize the image
+    image = cv2.resize(image, size)
+    img_array = image / 255.0  # Normalizar la imagen
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
 st.title("Image Classification with CNN - Made with love by Hardik")
 st.write("Simply upload a picture, and our advanced AI model will tell you if it's a building, forest, glacier, mountain, sea, or street.")
 
-# File uploader
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+# Subir archivo
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+    # Leer la imagen usando OpenCV
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    image = cv2.imdecode(file_bytes, 1)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
-
-    # Preprocess the image
+    
+    # Preprocesar la imagen
     img_array = preprocess_image(image)
-
-    # Predict the class
-    prediction = model.predict(img_array)
-    predicted_class = class_names[np.argmax(prediction)]
-    confidence = np.max(prediction)
-
+    
+    # Hacer predicciones
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+    predictions = interpreter.get_tensor(output_details[0]['index'])
+    
+    # Obtener la clase con mayor probabilidad
+    predicted_class = class_names[np.argmax(predictions)]
     st.write(f"Prediction: {predicted_class}")
-    st.write(f"Confidence: {confidence:.2f}")
-
-
