fd143cdfc65ba2ddb7733935b5fd8cfdb8f9a8b6
Martín Badino <151761027+MartoBadi@users.noreply.github.com>
2024-10-13 11:17:43 -0300

    Agregué  img_array = img_array.astype(np.float32) en app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def preprocess_image(image):
     image = np.resize(image, (size[0], size[1], 3))  # Redimensionar la imagen
     img_array = image / 255.0  # Normalizar la imagen
     img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array.astype(np.float32)  # Asegurarse de que la imagen sea de tipo FLOAT32
     return img_array
 
 st.title("Image Classification with CNN - Made with love by Hardik")
