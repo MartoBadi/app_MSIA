cbc7ccbbed8ce3f3f58b6dbc9ded84c6a7e3b791
Martín Badino <151761027+MartoBadi@users.noreply.github.com>
2024-10-16 11:48:23 -0300

    Actualizar app_testing.py

--- app_testing.py
+++ app_testing.py
@@ -6,8 +6,10 @@ import numpy as np
 import os
 
 # Cargar el modelo TFLite
-interpreter = tf.lite.Interpreter(model_path='modelLautaro.tflite')
+interpreter = tf.lite.Interpreter(model_path='model.tflite')
 interpreter.allocate_tensors()
+
+repo_path = os.path.dirname(os.path.abspath(__file__))
 
 # Obtener detalles de entrada y salida
 input_details = interpreter.get_input_details()
@@ -23,41 +25,67 @@ def preprocess_image(image):
     image = np.resize(image, (size[0], size[1], 3))  # Redimensionar la imagen
     img_array = image / 255.0  # Normalizar la imagen
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array.astype(np.float32)  # Asegurarse de que la imagen sea de tipo FLOAT32
+    img_array = img_array.astype(np.float32) 
     return img_array
 
-# Función para evaluar el conjunto de prueba
-def evaluate_test_set(test_dir):
-    correct_predictions = 0
-    total_predictions = 0
+# Función para buscar la carpeta de la imagen
+def find_image_folder(image_name, base_dir=''):
+    for root, dirs, files in os.walk(base_dir):
+        if image_name in files:
+            return os.path.basename(root)
+    return "Unknown"
 
-    for class_name in os.listdir(test_dir):
-        class_dir = os.path.join(test_dir, class_name)
-        if os.path.isdir(class_dir):
-            for image_name in os.listdir(class_dir):
-                image_path = os.path.join(class_dir, image_name)
-                image = mpimg.imread(image_path)
-                img_array = preprocess_image(image)
-                
-                # Hacer predicciones
-                interpreter.set_tensor(input_details[0]['index'], img_array)
-                interpreter.invoke()
-                predictions = interpreter.get_tensor(output_details[0]['index'])
-                
-                # Obtener la clase con mayor probabilidad
-                predicted_class = class_names[np.argmax(predictions)]
-                
-                # Comparar con la etiqueta real
-                if predicted_class == class_name:
-                    correct_predictions += 1
-                total_predictions += 1
+st.title("Clasificación de imágenes de maravillas del mundo")
+st.write("Este sitio web fue creado para la materia Modelizado de Sistemas de IA de la Tecnicatura Superior en Ciencias de Datos e Inteligencia Artificial del IFTS 18. La idea es que subas una imagen de uno de las siguientes maravillas del mundo: burj_khalifa, chichen_itza, christ the reedemer, eiffel_tower, great_wall_of_china, machu_pichu, pyramids_of_giza, roman_colosseum, statue_of_liberty, stonehenge, taj_mahal, venezuela_angel_falls y el modelo te dirá qué maravilla aparece en la imagen. ¡Diviértete!")
 
-    accuracy = correct_predictions / total_predictions
-    return accuracy
+# Subir archivo
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-# Directorio del conjunto de prueba
-test_dir = './Wonders of World/test'
+if uploaded_file is not None:
+    # Leer la imagen usando Matplotlib
+    image = mpimg.imread(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    
+    # Preprocesar la imagen
+    img_array = preprocess_image(image)
+    
+    # Hacer predicciones
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+    predictions = interpreter.get_tensor(output_details[0]['index'])
+    
+    # Obtener la clase con mayor probabilidad
+    predicted_class = class_names[np.argmax(predictions)]
 
-# Evaluar el conjunto de prueba
-accuracy = evaluate_test_set(test_dir)
-st.write(f"Test set accuracy: {accuracy:.2f}")
+    image_name = uploaded_file.name
+    
+    # Buscar la carpeta de la imagen
+    real_class = find_image_folder(image_name, base_dir=repo_path)
+  
+    st.write(f"Real class: {real_class}")
+    st.write(f"Prediction: {predicted_class}")
+
+    # Inicializar el contador en session_state si no existe
+    
+    if 'correct_predictions' not in st.session_state:
+        st.session_state.correct_predictions = 0
+
+        # Mostrar el contador actualizado
+        if real_class == predicted_class:
+            st.session_state.correct_predictions += 1
+            st.write(f"Correct Predictions: {st.session_state.correct_predictions}")
+        else:            
+    # Mostrar el contador actualizado
+            st.write(f"Correct Predictions: {st.session_state.correct_predictions}")
+    
+    else:
+    
+    # Incrementar el contador si la clase real es igual a la clase predicha
+        if real_class == predicted_class:
+            st.session_state.correct_predictions += 1
+            st.write(f"Correct Predictions: {st.session_state.correct_predictions}")
+        else:            
+    # Mostrar el contador actualizado
+            st.write(f"Correct Predictions: {st.session_state.correct_predictions}")
