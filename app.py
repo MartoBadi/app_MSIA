2d7a815784f93982bd32e1f86488b0d8a6601906
sailornix <58754786+sailornix@users.noreply.github.com>
2024-10-16 20:29:12 -0300

    60%

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class_names = ['Burj Khalifa', 'Chichén Itzá', 'Cristo Redentor', 'Torre Eiffe
                'Estatua de la Libertad', 'Stonehenge', 'Taj Mahal', 'Salto Ángel']
 
 # Definir umbral de confianza (por ejemplo, 80%)
-confidence_threshold = 0.7
+confidence_threshold = 0.6
 
 # Preprocesar la imagen subida
 def preprocess_image(image):
